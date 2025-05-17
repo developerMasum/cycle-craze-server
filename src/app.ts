fe59fe0e1492c6494b8ec88670840a145db448b4
developerMasum <developer.masum2001@gmail.com>
2025-05-17 10:15:12 +0600

    added cors

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,15 @@ import { paymentRoutes } from "./app/modules/payment/payment.routes";
 const app = express();
 
 app.use(bodyParser.json());
-app.use(cors());
+// app.use(cookieParser());
+
+// app.use(cors({ origin: 'http://localhost:5173s', credentials: true }));
+app.use(
+  cors({
+    origin: ["https://trendys.vercel.app", "http://localhost:5173"],
+    credentials: true,
+  })
+);
 
 app.use("/api/v1/product", productRoutes);
 app.use("/api/v1/order", orderRoutes);
