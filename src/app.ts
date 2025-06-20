4a87553f6b793a441da3446bb744ac1387d3eb8b
developerMasum <developer.masum2001@gmail.com>
2025-06-20 11:20:02 +0600

    cors

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["https://cycle-wave.vercel.app", "http://localhost:5173"],
     credentials: true,
   })
 );
